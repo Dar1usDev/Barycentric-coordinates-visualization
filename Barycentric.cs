fe62f9efea3f03cc5857a991e57092312ec8a507
Dmitry <Selesnjow@gmail.com>
2022-04-27 01:18:56 +1000

    Добавьте файлы проекта.

---
+++ Barycentric.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Barycentric_coordinates_visualization
+{
+    public class Barycentric
+    {
+        private static double[] Xcrds = new double[3];
+        private static double[] Ycrds = new double[3];
+        private static double S2;
+        private static double CalculateS2() => Xcrds[0] * Ycrds[1] + Xcrds[1] * Ycrds[2] + Xcrds[2] * Ycrds[0] 
+            - Xcrds[0] * Ycrds[2] - Xcrds[1] * Ycrds[0] - Xcrds[2] * Ycrds[1];
+
+        static public double Coord1(double x, double y) => (x * (Ycrds[1] - Ycrds[2]) + y * (Xcrds[2] - Xcrds[1]) 
+            + Xcrds[1] * Ycrds[2] - Xcrds[2] * Ycrds[1]) / S2;
+        static public double Coord2(double x, double y) => (x * (Ycrds[2] - Ycrds[0]) + y * (Xcrds[0] - Xcrds[2])
+            + Xcrds[2] * Ycrds[0] - Xcrds[0] * Ycrds[2]) / S2;
+        static public double Coord3(double x, double y) => (x * (Ycrds[0] - Ycrds[1]) + y * (Xcrds[1] - Xcrds[0]) 
+            + Xcrds[0] * Ycrds[1] - Xcrds[1] * Ycrds[0]) / S2;
+        static public double CenterOf(double _x1, double _x2) => (Math.Max(_x1, _x2) + Math.Min(_x1, _x2)) / 2;
+
+        /// <summary>
+        /// Get x1,y1,x2,y2,x3,y3 massive
+        /// </summary>
+        public static void SetCoordinates(double[] _coordinates)
+        {
+            Array.Copy(_coordinates, 0, Xcrds, 0, 3);
+            Array.Copy(_coordinates, 3, Ycrds, 0, 3);
+            S2 = CalculateS2();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Barycentric_coordinates_visualization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PrepareCanvas()
+        {
+            canvas.Children.Clear();
+            Barycentric.SetCoordinates(new double[6] { double.Parse(x1TextBox.Text), double.Parse(y1TextBox.Text), double.Parse(x2TextBox.Text),
+                double.Parse(y2TextBox.Text),double.Parse(x3TextBox.Text),double.Parse(y3TextBox.Text)});
+            DrawTriangle();
+        }
+
+        private void DrawTriangle()
+        {
+            Polyline triangle = new Polyline();
+            triangle.Points = new PointCollection();
+            triangle.Points.Add(new Point(double.Parse(x1TextBox.Text), double.Parse(y1TextBox.Text)));
+            triangle.Points.Add(new Point(double.Parse(x2TextBox.Text), double.Parse(y2TextBox.Text)));
+            triangle.Points.Add(new Point(double.Parse(x3TextBox.Text), double.Parse(y3TextBox.Text)));
+            triangle.Points.Add(new Point(double.Parse(x1TextBox.Text), double.Parse(y1TextBox.Text)));
+            triangle.Stroke = Brushes.Black;
+            triangle.StrokeThickness = 5;
+            canvas.Children.Add(triangle);
+        }
+
+        public void DrawPoint(double x, double y, double value)
+        {
+            PrepareCanvas();
+            Ellipse point = new Ellipse();
+            point.Margin = new Thickness(x, y, 0, 0);
+            int r = 10;
+            point.Width = r;
+            point.Height = r;
+            point.Fill = new SolidColorBrush(Color.FromArgb(255, (byte)(255 * value), 185, 0));
+            canvas.Children.Add(point);
+            ksiValueTextBox.Text = value.ToString();
+        }
+
+        private void drawPointButton_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(xTextBox.Text);
+            double y = double.Parse(yTextBox.Text);
+            DrawPoint(x, y, Barycentric.Coord1(x,y));
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+        }
+
+        private void ksi1Button_Click(object sender, RoutedEventArgs e)
+        {
+            PrepareCanvas();
+        }
+
+        private void ksi2Button_Click(object sender, RoutedEventArgs e)
+        {
+            PrepareCanvas();
+        }
+
+        private void ksi3Button_Click(object sender, RoutedEventArgs e)
+        {
+            PrepareCanvas();
+        }
+    }
+}
